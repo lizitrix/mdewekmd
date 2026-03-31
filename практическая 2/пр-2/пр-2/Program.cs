4cd3850751d472c0bc82111b48304634a64c982c
lizitrix <emartynova110@gmail.com>
2026-04-01 01:00:52 +0300

    Add files via upload

---
+++ практическая 2/пр-2/пр-2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите размеры отверстия A и B:");
+            double A = Convert.ToDouble(Console.ReadLine());
+            double B = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Введите размеры кирпича x, y, z:");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+
+            if (x <= A && y <= B || x <= B && y <= A)
+            {
+                Console.WriteLine("Кирпич ПРОЙДЕТ");
+            }
+            else if (x <= A && z <= B || x <= B && z <= A)
+            {
+                Console.WriteLine("Кирпич ПРОЙДЕТ");
+            }
+            else if (y <= A && z <= B || y <= B && z <= A)
+            {
+                Console.WriteLine("Кирпич ПРОЙДЕТ");
+            }
+            else
+            {
+                Console.WriteLine("Кирпич НЕ ПРОЙДЕТ");
+            }
+        }
+    }
+}
---
+++ практическая 2/пр2/пр2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите x");
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = 0;
+
+            if (x > 1.1)
+                y = -x + 9;
+            if (x < -1.1)
+                y = Math.Pow(Math.Sin(3 * x), 2) / (Math.Pow(x, 4) + 1);
+
+            Console.WriteLine("Для x = {0:f4}", x);
+            Console.WriteLine("Результат = {0:f4}", y);
+        }
+    }
+}
