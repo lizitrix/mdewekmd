c1d8b0e51581ab59f0bb5e45f64d4abb6d232b76
lizitrix <emartynova110@gmail.com>
2026-04-01 00:58:39 +0300

    Add files via upload

---
+++ практичская 1/пр1/пр1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("введите х");
+            double x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите y");
+            double y = Convert.ToDouble(Console.ReadLine());
+
+            double z = Math.Pow(x, Math.Pow(y, 2) + 3 * y - 5) + Math.Atan(Math.Pow(y, 4));
+
+            Console.WriteLine("для х={0:f4} и у={1:f4}", x, y);
+            Console.WriteLine("результат ={0:f4}", z);
+        }
+    }
+}
+
---
+++ практичская 1/пр_1/пр_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("введите высоту");
+            double H = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("введите радиус основания");
+            double r = Convert.ToDouble(Console.ReadLine());
+
+            double Vcone = 1.0 / 3.0 * Math.PI * Math.Pow(r, 2) * H;
+            double Vcyl = Math.PI * Math.Pow(r, 2) * H;
+
+            Console.WriteLine("для H={0:f4} и r={1:f4}", H, r);
+            Console.WriteLine("конус ={0:f4} и цилиндр ={0:f4}", Vcone, Vcyl);
+        }
+    }
+}
+
