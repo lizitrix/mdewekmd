5308025eabf216fecb425a8372b516ddf572b805
lizitrix <emartynova110@gmail.com>
2026-04-01 01:06:27 +0300

    Add files via upload

---
+++ практическая 4/зд1/зд1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите первое число: ");
+        string s1 = Console.ReadLine();
+
+        Console.Write("Введите второе число: ");
+        string s2 = Console.ReadLine();
+
+        string result = AddBigNumbers(s1, s2);
+        Console.WriteLine("Сумма = " + result);
+    }
+
+    static string AddBigNumbers(string a, string b)
+    {
+        if (a.Length < b.Length) return AddBigNumbers(b, a);
+
+        int carry = 0;
+        string result = "";
+        int diff = a.Length - b.Length;
+
+        for (int i = a.Length - 1; i >= 0; i--)
+        {
+            int digitA = a[i] - '0';
+            int digitB = (i - diff >= 0) ? b[i - diff] - '0' : 0;
+
+            int sum = digitA + digitB + carry;
+            result = (sum % 10) + result;
+            carry = sum / 10;
+        }
+
+        if (carry > 0) result = carry + result;
+        return result;
+    }
+}
---
+++ практическая 4/зд2/зд2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите начало отрезка a (>0): ");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите конец отрезка b: ");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Введите шаг h: ");
+        double h = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("\n----------------------");
+        Console.WriteLine("   x    |    F(x)    ");
+        Console.WriteLine("----------------------");
+
+        for (double x = a; x <= b + h / 2; x += h)
+        {
+            double F = 2 * Math.Cos(Math.Sqrt(x)) + 0.5 / x;
+            Console.WriteLine("{0,7:f4} | {1,10:f4}", x, F);
+        }
+
+        Console.WriteLine("----------------------");
+    }
+}
